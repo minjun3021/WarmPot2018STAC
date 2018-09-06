651d2a00ae9e1f6b03ab8a0caab5c69e9781b2f2
minjun3021 <minjun3021@naver.com>
2018-09-07 05:11:50 +0900

    startspeak

---
+++ app/src/main/java/com/example/kmj/warmpot_2018stac/SpeakFragment.java
@@ -0,0 +1,30 @@
+package com.example.kmj.warmpot_2018stac;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class SpeakFragment extends Fragment {
+    public static SpeakFragment newInstance() {
+        SpeakFragment fragment = new SpeakFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_speak, container, false);
+    }
+}
