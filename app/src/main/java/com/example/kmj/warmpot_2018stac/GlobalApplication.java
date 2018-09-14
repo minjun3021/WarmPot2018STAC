6b7efacbe9f80c9e3aabf5b2fa6a0f605738cc65
minjun3021 <minjun3021@naver.com>
2018-09-14 20:14:09 +0900

    kakaologin

---
+++ app/src/main/java/com/example/kmj/warmpot_2018stac/GlobalApplication.java
@@ -0,0 +1,72 @@
+package com.example.kmj.warmpot_2018stac;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.kakao.auth.ApprovalType;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.ISessionConfig;
+import com.kakao.auth.KakaoAdapter;
+import com.kakao.auth.KakaoSDK;
+
+public class GlobalApplication extends Application {
+
+    // TODO KakaoSDKAdapter 클래스 static 지워줘도 작동함
+    private class KakaoSDKAdapter extends KakaoAdapter {
+        /**
+         * Session Config에 대해서는 default값들이 존재한다.
+         * 필요한 상황에서만 override해서 사용하면 됨.
+         * @return Session의 설정값.
+         */
+        @Override
+        public ISessionConfig getSessionConfig() {
+            return new ISessionConfig() {
+                @Override
+                public AuthType[] getAuthTypes() {
+                    return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
+                }
+
+                @Override
+                public boolean isUsingWebviewTimer() {
+                    return false;
+                }
+
+                @Override
+                public boolean isSecureMode() {
+                    return false;
+                }
+
+                @Override
+                public ApprovalType getApprovalType() {
+                    return ApprovalType.INDIVIDUAL;
+                }
+
+                @Override
+                public boolean isSaveFormData() {
+                    return true;
+                }
+            };
+        }
+
+        @Override
+        public IApplicationConfig getApplicationConfig() {
+            return new IApplicationConfig() {
+                @Override
+                public Context getApplicationContext() {
+                    //TODO Globalapplication.getApplicationContext하면 오류남 아래처럼 해줘야함
+                    return GlobalApplication.this.getApplicationContext();
+                }
+            };
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        KakaoSDK.init(new KakaoSDKAdapter());
+
+    }
+
+}
--- app/src/main/java/com/example/kmj/warmpot_2018stac/registerSelect.java
+++ app/src/main/java/com/example/kmj/warmpot_2018stac/registerSelect.java
@@ -4,54 +4,109 @@ import android.app.ActivityManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.List;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
 
 public class registerSelect extends AppCompatActivity {
     Button resbutton;
-    Button reskakao;
-
+    TextView already;
+    private SessionCallback callback;
     private BackPressCloseHandler backPressCloseHandler;
 
-    TextView already;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_select);
+        callback = new SessionCallback();
+
+        Session.getCurrentSession().addCallback(callback);
+
         backPressCloseHandler = new BackPressCloseHandler(this);
-        resbutton=findViewById(R.id.rs_btn);
-        reskakao=findViewById(R.id.rs_kakaobtn);
-        already=findViewById(R.id.rs_already);
+        resbutton = findViewById(R.id.rs_btn);
+        already = findViewById(R.id.rs_already);
         already.setText(R.string.register_select_text);
+
         resbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(registerSelect.this,registerActivity.class);
+                Intent intent = new Intent(registerSelect.this, registerActivity.class);
                 startActivity(intent);
             }
         });
-        reskakao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
         already.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(registerSelect.this,Login.class);
+                Intent intent = new Intent(registerSelect.this, Login.class);
                 startActivity(intent);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    public void requestMe() {
+        UserManagement.getInstance().me(new MeV2ResponseCallback() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+
+            }
+
+            @Override
+            public void onSuccess(MeV2Response result) {
+                // TODO 나중에 서버 연동할떄 서버에 보낼 access_token값
+                String access_token = Session.getCurrentSession().getTokenInfo().getAccessToken();
+                Log.e("token", access_token);
+            }
+        });
+    }
+
     @Override
     public void onBackPressed() {
         backPressCloseHandler.onBackPressed();
 
-            //super.onBackPressed();
+        //super.onBackPressed();
+
+    }
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            Log.i("KAKAO", "Session Opened");
+            requestMe();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if (exception != null) {
+                Logger.e(exception);
+            }
+        }
 
     }
 }
